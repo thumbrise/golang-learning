3e76c9f1f538f2ce5db5522e3c7e640303be0be1
ruslan-kokoev <ruslan.kokoev.1999@gmail.com>
2026-02-15 23:23:38 +0300

    wip: equal_row_and_column_pairs2

--- internal/algorithms_and_data_structures/equal_row_and_column_pairs/equal_row_and_column_pairs.go
+++ internal/algorithms_and_data_structures/equal_row_and_column_pairs/equal_row_and_column_pairs.go
@@ -31,3 +31,31 @@ func EqualPairs(grid [][]int) int {
 
 	return result
 }
+
+func EqualPairs2(grid [][]int) int {
+	rows := map[string]int{}
+	cols := map[string]int{}
+
+	for y := range grid {
+		row := ""
+		col := ""
+
+		for x := range grid[y] {
+			row += fmt.Sprintf("%d,", grid[y][x])
+			col += fmt.Sprintf("%d,", grid[x][y])
+		}
+
+		rows[row]++
+		cols[col]++
+	}
+
+	result := 0
+
+	for k, v := range rows {
+		if cols[k] > 0 {
+			result += v * cols[k]
+		}
+	}
+
+	return result
+}
--- internal/algorithms_and_data_structures/equal_row_and_column_pairs/equal_row_and_column_pairs_test.go
+++ internal/algorithms_and_data_structures/equal_row_and_column_pairs/equal_row_and_column_pairs_test.go
@@ -1,6 +1,7 @@
 package equal_row_and_column_pairs_test
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/thumbrise/golang-learning/internal/algorithms_and_data_structures/equal_row_and_column_pairs"
@@ -96,12 +97,45 @@ func TestEqualPairs(t *testing.T) {
 			want: 0,
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := equal_row_and_column_pairs.EqualPairs(tt.grid); got != tt.want {
 				t.Errorf("EqualPairs() = %v, want %v", got, tt.want)
 			}
 		})
+		t.Run(tt.name, func(t *testing.T) {
+			if got := equal_row_and_column_pairs.EqualPairs2(tt.grid); got != tt.want {
+				t.Errorf("EqualPairs2() = %v, want %v", got, tt.want)
+			}
+		})
 	}
+}
+
+func generateLargeGrid(size int) [][]int {
+	grid := make([][]int, size)
+	r := rand.New(rand.NewSource(42)) // фиксированный seed для воспроизводимости
+
+	for i := 0; i < size; i++ {
+		row := make([]int, size)
+		for j := 0; j < size; j++ {
+			// числа от 1 до 10^5 как в условии
+			row[j] = r.Intn(100000) + 1
+		}
+		grid[i] = row
+	}
+	return grid
+}
+
+func BenchmarkEqualPairs(b *testing.B) {
+	grid := generateLargeGrid(200)
+	b.Run("String Version", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			equal_row_and_column_pairs.EqualPairs(grid)
+		}
+	})
+	b.Run("Slice Version", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			equal_row_and_column_pairs.EqualPairs2(grid)
+		}
+	})
 }
