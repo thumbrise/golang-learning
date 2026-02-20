ab3c9750eada843d238f26694870f1fe546f2436
ruslan-kokoev <ruslan.kokoev.1999@gmail.com>
2026-02-20 16:27:12 +0300

    feat: LeetCode - 735. Asteroid Collision

--- internal/leetcode/asteroid_collision/asteroid_collision.go
+++ internal/leetcode/asteroid_collision/asteroid_collision.go
@@ -1,5 +1,30 @@
 package asteroid_collision
 
 func AsteroidCollision(asteroids []int) []int {
-	return nil
+	result := make([]int, 0, len(asteroids))
+	result = append(result, asteroids[0])
+	for i := 1; i < len(asteroids); i++ {
+		a := result[len(result)-1]
+		b := asteroids[i]
+
+		if a*b > 0 {
+			// same direction
+			// push new
+		}
+
+		if a > b {
+			// old win
+			// skip new
+		}
+
+		if a == -b {
+			// both explodes
+			// pop old and skip new
+		}
+
+		// new win
+		// pop old and push new
+	}
+
+	return result
 }
---
+++ internal/leetcode/asteroid_collision/asteroid_collision_test.go
@@ -0,0 +1,53 @@
+package asteroid_collision
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAsteroidCollision(t *testing.T) {
+	type args struct {
+		asteroids []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "test 1",
+			args: args{
+				asteroids: []int{5, 10, -5},
+			},
+			want: []int{5, 10},
+		},
+		{
+			name: "test 2",
+			args: args{
+				asteroids: []int{8, -8},
+			},
+			want: []int{},
+		},
+		{
+			name: "test 3",
+			args: args{
+				asteroids: []int{10, 2, -5},
+			},
+			want: []int{10},
+		},
+		{
+			name: "test 4",
+			args: args{
+				asteroids: []int{3, 5, -6, 2, -1, 4},
+			},
+			want: []int{-6, 2, 4},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AsteroidCollision(tt.args.asteroids); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AsteroidCollision() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
