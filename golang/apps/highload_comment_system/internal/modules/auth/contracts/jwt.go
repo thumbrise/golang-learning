b60f7b1a00bf01d0239eedec022cae30d3c84947
ruslan-kokoev <ruslan.kokoev.1999@gmail.com>
2026-03-02 01:06:12 +0300

    wip: golang demo application
    - Абстрагирование от FX

--- golang/apps/highload_comment_system/internal/modules/auth/contracts/jwt.go
+++
@@ -1,9 +0,0 @@
-package contracts
-
-import "github.com/gin-gonic/gin"
-
-type JWT interface {
-	ClaimsFromGinContext(ctx *gin.Context) *JWTClaims
-	Issue(userID int, username string, email string, roles []string) (*JWTPair, error)
-	Parse(tokenString string) (*JWTClaims, error)
-}
