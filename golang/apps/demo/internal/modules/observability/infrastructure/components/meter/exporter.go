be2239f2aa5d46af0810ed8df814d3c2c5667c40
ruslan-kokoev <ruslan.kokoev.1999@gmail.com>
2026-03-06 18:57:40 +0300

    wip: golang demo application, comments system
    - Добавлен экспортер для метрик

---
+++ golang/apps/demo/internal/modules/observability/infrastructure/components/meter/exporter.go
@@ -0,0 +1,16 @@
+package meter
+
+import (
+	"context"
+
+	"github.com/thumbrise/demo/golang-demo/internal/modules/observability/infrastructure"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+)
+
+func NewExporter(ctx context.Context, cfg infrastructure.OTLPConfig) (*otlpmetricgrpc.Exporter, error) {
+	return otlpmetricgrpc.New(ctx,
+		otlpmetricgrpc.WithInsecure(),
+		otlpmetricgrpc.WithEndpoint(cfg.URL),
+		otlpmetricgrpc.WithCompressor("gzip"),
+	)
+}
--- golang/apps/demo/internal/modules/observability/infrastructure/components/meter/sdkprovider.go
+++ golang/apps/demo/internal/modules/observability/infrastructure/components/meter/sdkprovider.go
@@ -1,9 +1,18 @@
 package meter
 
 import (
+	"time"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	sdkmeter "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
 )
 
-func NewOTELSDKProvider() *sdkmeter.MeterProvider {
-	return sdkmeter.NewMeterProvider()
+func NewOTELSDKProvider(res *resource.Resource, exp *otlpmetricgrpc.Exporter) *sdkmeter.MeterProvider {
+	return sdkmeter.NewMeterProvider(
+		sdkmeter.WithResource(res),
+		sdkmeter.WithReader(sdkmeter.NewPeriodicReader(exp,
+			sdkmeter.WithInterval(10*time.Second),
+		)),
+	)
 }
