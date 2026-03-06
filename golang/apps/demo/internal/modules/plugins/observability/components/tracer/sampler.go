16ec3e95221c74aae0381fd33a39bbdca35718a8
ruslan-kokoev <ruslan.kokoev.1999@gmail.com>
2026-03-07 02:09:25 +0300

    style[golang|apps|demo]: OTEL observability

--- golang/apps/demo/internal/modules/plugins/observability/components/tracer/sampler.go
+++ golang/apps/demo/internal/modules/plugins/observability/components/tracer/sampler.go
@@ -2,7 +2,7 @@ package tracer
 
 import sdktrace "go.opentelemetry.io/otel/sdk/trace"
 
-func NewOTELSampler() sdktrace.Sampler {
+func NewOTELSampler() sdktrace.Sampler { //nolint:ireturn //specific case
 	return sdktrace.ParentBased(
 		sdktrace.TraceIDRatioBased(1.0),
 	)
