700e142f697b1407c64fe0c44dff7e6662a884a0
ruslan-kokoev <ruslan.kokoev.1999@gmail.com>
2026-03-07 23:25:33 +0300

    wip[golang|apps|demo]: Публикация комментариев

--- golang/apps/demo/internal/modules/comments/application/usecases/comments_command_produce.go
+++ golang/apps/demo/internal/modules/comments/application/usecases/comments_command_produce.go
@@ -21,7 +21,9 @@ func NewCommentsCommandProduce(logger *slog.Logger, redis *redis.Client) *Commen
 	return &CommentsCommandProduce{logger: logger, redis: redis}
 }
 
-type CommentsCommandProduceInput struct{}
+type CommentsCommandProduceInput struct {
+	Comments []string // получаем комментарии из входных данных
+}
 
 type CommentsCommandProduceOutput struct{}
 
@@ -30,41 +32,51 @@ func (c *CommentsCommandProduce) Handle(ctx context.Context, input CommentsComma
 	// TODO: Добавить консюмера
 	// TODO: Вынести общую логику стриминга в инфраструктуру
 	// TODO: Добавить возможность настройки количества горутин. Обязательно с shared limiter
+
 	limiter := rate.NewLimiter(rate.Limit(3000), 1)
 
-	for {
-		select {
-		case <-ctx.Done():
-			return &CommentsCommandProduceOutput{}, ctx.Err()
-		default:
-			err := limiter.Wait(ctx)
-			if err != nil {
-				continue
-			}
+	inputComments := input.Comments // получаем комментарии из входных данных
 
-			values := map[string]interface{}{
-				"uuid":      uuid.New().String(),
-				"timestamp": strconv.FormatInt(time.Now().Unix(), 10),
-				"content":   faker.Paragraph(),
-			}
-
-			result, err := c.redis.XAdd(ctx, &redis.XAddArgs{
-				Stream:     "comments_unprocessed",
-				NoMkStream: false,
-				ID:         "*",
-				Values:     values,
-			}).Result()
-			if err != nil {
-				c.logger.ErrorContext(ctx, "XAdd failed",
-					slog.String("error", err.Error()),
-					slog.Any("values", values),
-				)
-			} else {
-				c.logger.DebugContext(ctx, "XAdd succeeded",
-					slog.String("result", result),
-					slog.Any("values", values),
-				)
-			}
+	for _, comment := range inputComments {
+		values := map[string]interface{}{
+			"uuid":      uuid.New().String(),
+			"timestamp": strconv.FormatInt(time.Now().Unix(), 10),
+			"content":   faker.Paragraph(),
 		}
+
+		tx, err := c.redis.TxPipelined(ctx)
+		if err != nil {
+			return &CommentsCommandProduceOutput{}, err
+		}
+		defer tx.Close()
+
+		result, err := tx.XAdd(ctx, &redis.XAddArgs{
+			Stream:     "comments_unprocessed",
+			NoMkStream: false,
+			ID:         "*",
+			Values:     values,
+		}).Result()
+		if err != nil {
+			return &CommentsCommandProduceOutput{}, err
+		}
+
+		cacheKey := "comments:" + uuid.New().String()
+		err = tx.HSet(ctx, cacheKey, values).Err()
+		if err != nil {
+			return &CommentsCommandProduceOutput{}, err
+		}
+
+		// Commit transaction
+		_, err = tx.Exec(ctx)
+		if err != nil {
+			return &CommentsCommandProduceOutput{}, err
+		}
+
+		c.logger.DebugContext(ctx, "Comment saved successfully",
+			slog.String("result", result),
+			slog.Any("values", values),
+		)
+
+		return &CommentsCommandProduceOutput{}, nil
 	}
 }
